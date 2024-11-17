2924230f6ef1dc745488f5a85160d8cdd401334b
DELL <DELL@DESKTOP-DH12UP9>
2024-11-17 10:50:41 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lsttaille = New System.Windows.Forms.ComboBox()
+        Me.rb1 = New System.Windows.Forms.RadioButton()
+        Me.rb2 = New System.Windows.Forms.RadioButton()
+        Me.rb3 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.btn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lsttaille
+        '
+        Me.lsttaille.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lsttaille.FormattingEnabled = True
+        Me.lsttaille.Items.AddRange(New Object() {"Petite", "Moyenne", "Grande"})
+        Me.lsttaille.Location = New System.Drawing.Point(3, 95)
+        Me.lsttaille.Name = "lsttaille"
+        Me.lsttaille.Size = New System.Drawing.Size(272, 33)
+        Me.lsttaille.TabIndex = 1
+        '
+        'rb1
+        '
+        Me.rb1.AutoSize = True
+        Me.rb1.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb1.Location = New System.Drawing.Point(367, 99)
+        Me.rb1.Name = "rb1"
+        Me.rb1.Size = New System.Drawing.Size(154, 31)
+        Me.rb1.TabIndex = 2
+        Me.rb1.TabStop = True
+        Me.rb1.Text = "croute fine"
+        Me.rb1.UseVisualStyleBackColor = True
+        '
+        'rb2
+        '
+        Me.rb2.AutoSize = True
+        Me.rb2.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb2.Location = New System.Drawing.Point(367, 153)
+        Me.rb2.Name = "rb2"
+        Me.rb2.Size = New System.Drawing.Size(219, 31)
+        Me.rb2.TabIndex = 3
+        Me.rb2.TabStop = True
+        Me.rb2.Text = "croute classique"
+        Me.rb2.UseVisualStyleBackColor = True
+        '
+        'rb3
+        '
+        Me.rb3.AutoSize = True
+        Me.rb3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb3.Location = New System.Drawing.Point(367, 209)
+        Me.rb3.Name = "rb3"
+        Me.rb3.Size = New System.Drawing.Size(170, 28)
+        Me.rb3.TabIndex = 4
+        Me.rb3.TabStop = True
+        Me.rb3.Text = "croute épaisse"
+        Me.rb3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(707, 99)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(179, 31)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "champinions"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox2.Location = New System.Drawing.Point(707, 153)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(101, 31)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "olives"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox3.Location = New System.Drawing.Point(707, 206)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(133, 31)
+        Me.CheckBox3.TabIndex = 7
+        Me.CheckBox3.Text = "poivrons"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Font = New System.Drawing.Font("Arial", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox4.Location = New System.Drawing.Point(707, 263)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(322, 31)
+        Me.CheckBox4.TabIndex = 8
+        Me.CheckBox4.Text = "frommage supplémentaire"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'btn
+        '
+        Me.btn.BackColor = System.Drawing.Color.Beige
+        Me.btn.Font = New System.Drawing.Font("Arial", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn.Location = New System.Drawing.Point(367, 396)
+        Me.btn.Name = "btn"
+        Me.btn.Size = New System.Drawing.Size(427, 68)
+        Me.btn.TabIndex = 9
+        Me.btn.Text = "Afficher le récapitulatif"
+        Me.btn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(278, 40)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Taille de la pizza"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(350, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(257, 40)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Type de croute"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(654, 43)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(546, 40)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Les ingrédients supplémentaires"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.ClientSize = New System.Drawing.Size(1317, 521)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.rb3)
+        Me.Controls.Add(Me.rb2)
+        Me.Controls.Add(Me.rb1)
+        Me.Controls.Add(Me.lsttaille)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lsttaille As ComboBox
+    Friend WithEvents rb1 As RadioButton
+    Friend WithEvents rb2 As RadioButton
+    Friend WithEvents rb3 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents btn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Private Sub lsttaille_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lsttaille.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btn_Click(sender As Object, e As EventArgs) Handles btn.Click
+        Dim taille = lsttaille.SelectedItem.ToString()
+        Dim ingredient As String = ""
+        If rb1.Checked Then
+            ingredient = "croute fine"
+        ElseIf rb2.Checked Then
+            ingredient = " croute classique"
+        ElseIf rb3.Checked Then
+            ingredient = "croute épaisse"
+        End If
+        Dim ingrsupp As String = ""
+        If CheckBox1.Checked Then
+            ingrsupp = "champinions"
+        ElseIf CheckBox2.Checked Then
+            ingrsupp = "olives"
+        ElseIf CheckBox3.Checked Then
+            ingrsupp = "poivrons"
+        ElseIf CheckBox4.Checked Then
+            ingrsupp = "frommage supplémentaire"
+        End If
+        Dim msg As String = "taille:  " + taille + "  " + "type de croute :  " + ingredient + "   " + "ingredients supplémentaire:  " + ingrsupp
+        MessageBox.Show(msg)
+    End Sub
+End Class
